55b8ea9ace3859a030e5de96d233f5a75a167eee
simbecode <simbecode@gmail.com>
2024-12-24 15:59:29 +0900

    2024_12_24_add_icon

--- ver_2/main.py
+++ ver_2/main.py
@@ -1,10 +1,8 @@
-# main.py
 import sys
 import os
 import logging
-from PyQt5.QtWidgets import QApplication, QSplashScreen
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon  # QIcon 모듈 추가
 from data_display_gui import DataDisplayGUI
 from data_receiver import DataReceiver
 from data_storage import DataStorage
@@ -12,16 +10,7 @@ from port_settings_gui import PortSettingsGUI
 from serial_port_manager import SerialPortManager
 from queue import Queue
 from custom_timed_rotating_file_handler import CustomTimedRotatingFileHandler
-import time
 
-def resource_path(relative_path):
-    """PyInstaller 환경과 개발 환경에서 리소스 경로를 처리."""
-    if hasattr(sys, '_MEIPASS'):
-        # PyInstaller로 빌드된 경우 실행 환경 디렉토리
-        return os.path.join(sys._MEIPASS, relative_path)
-    else:
-        # 개발 환경에서 상대 경로
-        return os.path.join(os.path.abspath("."), relative_path)
 
 def setup_logging():
     # 로그를 저장할 기본 디렉토리 설정
@@ -50,21 +39,20 @@ def setup_logging():
     logger.addHandler(handler)
 
 def main():
-    # 프로그램 초기화: 가장 먼저 스플래시 표시
-    app = QApplication(sys.argv)
-    splash_pix = QPixmap(resource_path('logo.png'))
-    splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
-    splash.show()
-    splash.showMessage("프로그램 로딩 중...", Qt.AlignBottom | Qt.AlignCenter, Qt.white)
-    app.processEvents()  # 스플래시 화면 즉시 표시
-
     # 로그 설정
     setup_logging()
-    logging.info("프로그램이 시작되었습니다.")
 
+    # 프로그램 초기화
+    logging.info("프로그램이 시작되었습니다.")
+    
+    app = QApplication(sys.argv)
+
+    # 아이콘 설정 (타이틀 바와 작업 표시줄 아이콘 설정)
+    app.setWindowIcon(QIcon("icon.ico"))  # 아이콘 파일 경로를 정확히 지정해야 합니다.
+    
     # 데이터 큐 생성
     data_queue = Queue()
-
+    
     # 기본 데이터 저장 경로 설정
     base_dir = r'C:\Sitech\data'
     if not os.path.exists(base_dir):
@@ -76,12 +64,8 @@ def main():
     # 포트 설정 GUI 표시
     spm = SerialPortManager()
     port_settings_gui = PortSettingsGUI(spm)
-
-    # 스플래시 종료: 설정 창 열릴 때
-    splash.close()
-
     if port_settings_gui.exec_() == 0:
-        logging.info("포트 설정이 취소되었습니다.")
+        # print("포트 설정이 취소되었습니다.")
         sys.exit()
 
     port_settings = port_settings_gui.port_settings
@@ -90,7 +74,7 @@ def main():
     temperature_source = port_settings_gui.temperature_source
 
     if not port_settings:
-        logging.info("포트 설정이 없습니다.")
+        # print("포트 설정이 없습니다.")
         sys.exit()
 
     # 데이터 수신 객체 생성
@@ -105,11 +89,10 @@ def main():
     def on_exit():
         data_receiver.stop()
         data_receiver.join()
-        ds.close()
+        ds.close() 
 
     app.aboutToQuit.connect(on_exit)
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    # 스플래시를 가장 먼저 표시
     main()
